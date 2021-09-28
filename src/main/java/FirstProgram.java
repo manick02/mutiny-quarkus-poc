4e211cdc8b3db8eb607447fd4392cb189eb789d5
cb-manick <80755357+cb-manick@users.noreply.github.com>
2021-09-28 17:59:02 +0530

    First reactive program with mutiny

---
+++ src/main/java/FirstProgram.java
@@ -0,0 +1,13 @@
+import io.smallrye.mutiny.Uni;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        Uni.createFrom().item("hello")
+                .onItem().transform(item -> item + " mutiny")
+                .onItem().transform(String::toUpperCase)
+                .subscribe().with(
+                        item -> System.out.println(">> " + item));
+    }
+}
+
