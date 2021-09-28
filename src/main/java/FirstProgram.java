8628840102e09ffbec314bab938b6129e0e67fe9
manick02 <manickavasagam.sundaram@gmail.com>
2021-09-28 18:22:16 +0530

    First reactive program with mutiny

--- src/main/java/FirstProgram.java
+++ src/main/java/FirstProgram.java
@@ -1,3 +1,4 @@
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 public class FirstProgram {
@@ -8,6 +9,11 @@ public class FirstProgram {
                 .onItem().transform(String::toUpperCase)
                 .subscribe().with(
                         item -> System.out.println(">> " + item));
+        Multi.createFrom().items(1, 2, 3, 4, 5)
+                .onItem().transform(i -> i * 2)
+                .select().first(3)
+                .onFailure().recoverWithItem(0)
+                .subscribe().with(System.out::println);
     }
 }
 
